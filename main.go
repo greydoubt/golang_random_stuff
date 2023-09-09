cd9c532a9c1b15392f8bb90e8c4eac0ce0f27292
greydoubt <43443470+greydoubt@users.noreply.github.com>
2023-09-09 03:21:09 +0000

    cleaner code

--- main.go
+++ main.go
@@ -1,25 +1,36 @@
+
 package main
 
 import "fmt"
 
+// Animal interface to be implemented by all types of animals
 type Animal interface {
     Speak() string
 }
 
+// Dog struct representing a dog
 type Dog struct {
     Name string
 }
 
+// Speak method for a Dog
 func (d Dog) Speak() string {
     return fmt.Sprintf("%s says Woof!", d.Name)
 }
 
+// Cat struct representing a cat
 type Cat struct {
     Name string
 }
 
+// Speak method for a Cat
 func (c Cat) Speak() string {
     return fmt.Sprintf("%s says Meow!", c.Name)
+}
+
+// function to get a speaking animal's sound
+func speakingAnimal(a Animal) {
+    fmt.Println(a.Speak())
 }
 
 func main() {
@@ -28,7 +39,8 @@ func main() {
 
     animals := []Animal{dog, cat}
 
+    // Iterate over the slice of animals, printing each one's Speak() string.
     for _, animal := range animals {
-        fmt.Println(animal.Speak())
+        speakingAnimal(animal)
     }
 }
